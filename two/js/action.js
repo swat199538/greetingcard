7dfeb0c5270fc5fa0aacf9ee507266cc5e0cf536
codingFish <wangliang199538@live.com>
2017-02-21 16:42:03 +0800

    第一页贺卡效果全部完成

--- two/js/action.js
+++ two/js/action.js
@@ -5,6 +5,9 @@
 var top_ayaka = document.getElementById('top_ayaka');
 var bottom_ayaka = document.getElementById('bottom_ayaka');
 var right_balloon = document.getElementById('right_balloon');
+var left_balloon = document.getElementById('left_balloon');
+var bottom_balloon = document.getElementById('bottom_balloon');
+var happy_birthday = document.getElementById('happy_birthday');
 /*移除出现动画，添加持续动画*/
 //头部彩花
 top_ayaka.addEventListener("animationend", function () {
@@ -21,5 +24,19 @@ right_balloon.addEventListener("animationend", function () {
     $("#right_balloon").removeClass('right_balloon_appear');
     $("#right_balloon").addClass('right_balloon_keep');
 });
-
+//左边气球
+left_balloon.addEventListener("animationend", function () {
+    $("#left_balloon").removeClass('left_balloon_appear');
+    $("#left_balloon").addClass('left_balloon_keep');
+});
+//底部气球
+bottom_balloon.addEventListener("animationend", function () {
+   $("#bottom_balloon").removeClass('bottom_balloon_appear');
+   $("#bottom_balloon").addClass('bottom_balloon_keep');
+   $("#happy_birthday").addClass('happy_birthday_appear');
+});
+//happy_birthday
+happy_birthday.addEventListener("animationend", function () {
+   $("#cake").addClass('cake_appear')
+});
 
