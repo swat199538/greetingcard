626cd83a34b4d1604f6dcc20b893e38235c7f216
wangl <wangliang199538@live.com>
2017-03-02 21:45:14 +0800

    打字机写成了一个对象。

--- two/js/typewriter.js
+++ two/js/typewriter.js
@@ -1,18 +1,57 @@
 /**
  * Created by wangl on 2017/3/2.
  */
-var contents = document.getElementById('font1').innerHTML;
-document.getElementById('font1').innerHTML="";
-var max = contents.length;
-var index=0;
-function display_word(content) {
-    if (index<max){
-        var old_font = document.getElementById('font1').innerHTML;
-        document.getElementById('font1').innerHTML= old_font+content[index]
-        index++;
-        console.log('执行了！');
-    } else {
+;!function (win) {
+    var interval;
+    var index = 0;
+    var word_index = 0;
 
+    //打字机显示文字
+    function show_word(param) {
+        console.log(index);
+        console.log(param.length);
+        if (index <= param.length-1) {
+            var id = '#'+param[index]['id'];
+            var old = $(id).html();
+            var word = param[index]['content'];
+            var new_word = old + word[word_index]
+            $(id).html(new_word);
+            console.log(id);
+            word_index+=1;
+            if (word_index+1 > param[index]['max']){
+                index+=1;
+            }
+            console.log('显示一个字')
+        } else {
+            console.log('打字机关闭了');
+            clearInterval(interval);
+            interval = '';
+            index = 0;
+            word_index = 0;
+        }
     }
-}
-setInterval("display_word(contents)", 1000);
+
+    win.TypeWriter = new Object()
+
+    //打字机绑定
+    TypeWriter.bind_note = function (param) {
+        var broswer = new Array();
+        for(var x in param){
+            var id = '#'+param[x];
+            broswer[x] = [];
+            broswer[x]['id'] = param[x];
+            broswer[x]['content'] = $(id).html();
+            $(id).html('');
+            broswer[x]['max'] = broswer[x]['content'].length;
+        }
+        return broswer;
+    }
+
+    //打字机启动
+    TypeWriter.start = function (param, time) {
+        interval = setInterval(show_word, time, param);
+    }
+
+
+
+}(window);
